697b29beba744f889542dc9ae34f6d1180a45da7
Mustafa Çavuşoğlu <65891055+mustafacavusoglu@users.noreply.github.com>
2022-04-06 12:19:46 +0300

    Update slice_with_overlay.py

--- slice_with_overlay.py
+++ slice_with_overlay.py
@@ -22,15 +22,34 @@ out_y = 128
 row = 2969
 col = 1413
 
-for  i in range(0, row - out_x + 1, out_x):
-    for j in range(0, col - out_y + 1, stride_y):
-        print(str(i) + ':' + str(i+stride_x) + ',' + str(j) + ':' + str(j+out_x))
+for i in range(0, row  + 1, stride_x):
+    for j in range(0, col + 1, stride_y):
+        if i+stride_x > row:
+            
+            if j+stride_y > col:
+                #print('1. if i n ifi')
+                plt.imsave(str(row-out_x) + '_' + str(row) + '_' + str(col-out_y) + '_' + str(col) + '.png',img[row-out_x:row, col-out_y:col,:])
+                print(str(row-out_x) + ':' + str(row) + ',' + str(col-out_y) + ':' + str(col))
+            else:
+                #print('1. if in else i')
+                plt.imsave(str(row-out_x) + '_' + str(row) + '_' + str(j) + '_' + str(j+out_y) + '.png',img[row-out_x:row, j:j+out_y,:])
+                print(str(row-out_x) + ':' + str(row) + ',' + str(j) + ':' + str(j+out_x))
+
+
+        elif j+stride_y > col:
+            
+            if i+stride_x > row:
+                #print('elifin ifi')
+                plt.imsave(str(row-out_x) + ':' + str(row) + ',' + str(col-out_y) + ':' + str(col) + '.png',img[row-out_x:row, col-out_y:col,:])
+                print(str(row-out_x) + ':' + str(row) + ',' + str(col-out_y) + ':' + str(col))
+            else:
+                #print('2. ifin else i')
+                plt.imsave(str(i) + '_' + str(i+out_x) + '_' + str(col-out_y) + '_' + str(col) + '.png',img[i:i+out_x, col-out_y:col,:])
+                print(str(i) + ':' + str(i+stride_x) + ',' + str(col-out_y) + ':' + str(col))
+
+        else:
+            #print('else')
+            plt.imsave(str(i) + '_' + str(i+out_x) + '_' + str(j) + '_' + str(j+out_y) + '.png',img[i:i+out_x, j:j+out_y,:])
+            print(str(i) + ':' + str(i+stride_x) + ',' + str(j) + ':' + str(j+out_x))
+        
     print('-------')
-
-
-"""
-num_images = [((y – b) // s_y)+1] * [((x – a) // s_x) + 1]
-"""
-
-result = ((col - out_y)// stride_y + 1) * ((row - out_x + 1) // stride_x + 1)
-print('Number of Images: {}'.format(result))
